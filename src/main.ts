6630575f8bbf1065efafbc1d0c12cf3b8d780bef
rahultripathi21 <tripathirahul158@gmail.com>
2026-05-25 01:34:42 +0530

    added port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = +process.env.PORT || 3000;
+  await app.listen(port, () => {
+    Logger.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
